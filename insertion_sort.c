636feca31a6ab73f596e5039c20d6da6516def71
Joel Kalos <jdk138@case.edu>
2016-12-13 23:33:23 -0500

    new parallel insertion sort

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
+#include <semaphore.h>
 
 #include "sorting_algorithms.h"
 
@@ -31,28 +32,96 @@ uint64_t insertion_sort (int *a, int n) {
     return diff;
 }
 
+struct arg_struct {
+    int *a;
+    int n;
+    struct node *x;
+};
+
+struct node {
+  int val;
+  struct node *next;
+};
+
+void *insertion_sort_thread(void *arguments){
+    struct arg_struct *args = arguments;
+    int *a = args -> a;
+    int n = args -> n;
+    struct node *x = args -> x;
+    struct node iterator = x;
+    struct node new_val = malloc(sizeof(struct node))
+
+    int i;
+    for (i = 0; i < n; i++) {
+        new_val.val = a[i];
+        new_val.next = NULL;
+        sem_wait(&mutex);
+
+        while(iterator -> next != NULL && iterator -> next -> val < a[i])
+            iterator = iterator -> next;
+
+        if (iterator -> next == NULL) {
+            iterator.next = new_val;
+        } else {
+            new_val.next = iterator -> next;
+            iterator.next = new_val;
+        }
+
+        sem_post(&mutex);
+    }
+
+}
+
 /** Sorts an input array using insertion sort and parallel processing
  *  Returns the time taken to sort the array */
 uint64_t insertion_sort_parallel (int *a, int n) {
-    struct timespec start, end;
-    uint64_t diff;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    
-    size_t i, j;
-    #pragma omp parallel for
-    for (i = 1; i < n; ++i) {
-        int tmp = a[i];
-        j = i;
-        #pragma omp critical
-        while (j > 0 && tmp < a[j - 1]) {
-            a[j] = a[j - 1];
-            --j;
-        }
-        a[j] = tmp;
+    pthread_t tid1, tid2;
+    struct arg_struct args1, args2;
+    sem_t mutex;
+    struct node *x = malloc(sizeof(struct node));
+
+    if(sem_init(&mutex, 0, 1) < 0) {
+        fprintf(stderr, "ERROR: could not initialize &semaphore.\n");
+        exit(0);
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
-    return diff;
+    x.val = a[0]
+
+    if (n < 2)
+        return 0;
+    int m = n / 2;
+
+    args1.a = a + 1;
+    args1.n = m - 1;
+    args1.x = x;
+    args2.a = a + m;
+    args2.n = n - m;
+    args2.x = x;
+
+    if (pthread_create(&tid1, NULL, &insertion_sort_thread, (void *)&args1) != 0) {
+        printf("Uh-oh!\n");
+        return -1;
+    }
+    if (pthread_create(&tid2, NULL, &insertion_sort_thread, (void *)&args2) != 0) {
+        printf("Uh-oh!\n");
+        return -1;
+    }
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+
+    struct node iterator = x;
+    struct node temp;
+    while(iterator -> next != NULL){
+        a[i] = iterator -> val;
+        temp = iterator;
+        iterator = iterator -> next;
+        free(temp);
+    }
+    free(x);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    return BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
 }
